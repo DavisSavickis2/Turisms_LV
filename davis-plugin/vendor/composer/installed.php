a2eaecfec3c2d7ebb32edfb056aa3e16f1c748f0
Davis <77230575+DavisSavickis2@users.noreply.github.com>
2021-03-25 17:13:03 +0200

    Add files via upload

---
+++ davis-plugin/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'davis_savickis/davis-plugin',
+  ),
+  'versions' => 
+  array (
+    'davis_savickis/davis-plugin' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
